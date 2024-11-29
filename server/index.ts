b3e3738f4155d3c3a98a4461c48c48b5cc52cf8b
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-11-29 18:45:40 +0200

    chore: add api controller

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import { logger } from "hono/logger"
 const api = new Hono().basePath('api')
 
 api.use(logger())
-api.get('/', (ctx) => ctx.json({ message: 'Hello from Hono!'}))
+
+api.get('/', (c) => c.json({ message: 'Hello from Hono!'}))
 
 export default api
