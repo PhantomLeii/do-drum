64071000bf7cbd0bc43c03a81cabf5f8dc89770b
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-11-30 13:19:53 +0200

    feat: add file-based routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
   root: './client',
-  plugins: [react()],
+  plugins: [
+    TanStackRouterVite({ routesDirectory: './client/routes', generatedRouteTree: './client/routeTree.gen.ts' }),
+    react()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './client'),
